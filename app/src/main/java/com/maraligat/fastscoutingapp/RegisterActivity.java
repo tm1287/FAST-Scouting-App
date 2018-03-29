c277154fe2f35ffbdd66979756fe2508da9d2df5
tm1287 <tejas.maraliga@gmail.com>
2018-03-29 14:38:29 -0400

    added register func

--- app/src/main/java/com/maraligat/fastscoutingapp/RegisterActivity.java
+++ app/src/main/java/com/maraligat/fastscoutingapp/RegisterActivity.java
@@ -2,23 +2,41 @@ package com.maraligat.fastscoutingapp;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
     private EditText r_UsernameEt;
     private EditText r_PasswordEt;
     private TextView r_SubmitTv;
     private TextView r_LoginTv;
+    private FirebaseAuth mAuth;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        mAuth = FirebaseAuth.getInstance();
         init();
     }
 
@@ -34,7 +52,28 @@ public class RegisterActivity extends AppCompatActivity {
         r_SubmitTv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String email = r_UsernameEt.getText().toString();
+                String password = r_PasswordEt.getText().toString();
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d("TAG", "createUserWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
 
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w("TAG", "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+
+                                // ...
+                            }
+                        });
             }
         });
 
